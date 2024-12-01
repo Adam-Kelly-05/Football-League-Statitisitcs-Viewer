50de6203b9a10a97521808c1a8258d012b0f75f4
Adam Kelly <S00252192@atu.ie>
2024-12-01 01:43:15 +0000

    Added Comments

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const leagueID = 357;
+const leagueID = 357; // The year and league id doesn't change, so they are constants
 const year = 2022;
 
 async function getTeamsInLeague() {
+    if (!document.getElementById('team-container')) { // this only runs if the teamsContainer hasn't been made yet
     const allTeams = await fetchTeamsInLeague();
     console.log(allTeams);
 
-    const teamContainer = document.createElement('div');
+    const teamContainer = document.createElement('div')
     teamContainer.className = 'team-container';
+    teamContainer.id = 'team-container'
     document.body.appendChild(teamContainer)
 
-    allTeams.response.forEach(i => {
+    allTeams.response.forEach(i => { // for every team in the response
         const teamCard = document.createElement('div');
         teamCard.className = 'team-card';
 
@@ -44,12 +46,13 @@ async function getTeamsInLeague() {
         const teamButton = document.createElement('button');
         teamButton.id = i.team.id;
         teamButton.textContent = "See Players";
-        teamButton.setAttribute('onclick', `getPlayersFromTeam(${i.team.id})`);
+        teamButton.setAttribute('onclick', `getPlayersFromTeam(${i.team.id})`); // make the button, when clicked it runs the getPlayersFromTeam function, with the team id passed in as a arguement
         teamDetails.appendChild(teamButton);
 
         teamCard.appendChild(teamDetails);
         teamContainer.appendChild(teamCard);
     });
+}
 };
 
 async function fetchTeamsInLeague() {
@@ -72,59 +75,63 @@ async function fetchTeamsInLeague() {
 }
 
 async function getPlayersFromTeam(teamID) {
-    if (!!document.getElementById('player-container-title')) {
-        document.getElementById('player-container-title').remove();
+    if (!!document.getElementById('playerContainerTitle')) { //  this only runs if the playerContainerTitle has been made
+        document.getElementById('playerContainerTitle').remove(); // delete the playerContainerTitle element
     }
     const playerContainerTitle = document.createElement('h1');
     playerContainerTitle.innerHTML = 'All Players';
-    playerContainerTitle.id = 'player-container-title';
+    playerContainerTitle.id = 'playerContainerTitle';
     document.body.appendChild(playerContainerTitle);
     
-    if (!!document.getElementById('player-container')) {
-        document.getElementById('player-container').remove()
+    if (!!document.getElementById('playerContainer')) { //  this only runs if the playerContainerTitle has been made
+        document.getElementById('playerContainer').remove() // delete the playerContainerTitle element
     }
     const playerContainer = document.createElement('div');
-    playerContainer.id = 'player-container';
+    playerContainer.id = 'playerContainer';
     playerContainer.className = 'player-container';
     document.body.appendChild(playerContainer);
 
     const teamInfo = await fetchPlayersFromTeam(teamID);
     console.log(teamInfo);
 
-    teamInfo.response.forEach(i => {
-        const playerCard = document.createElement('div');
-        playerCard.classList = 'player-card';
+    teamInfo.response.forEach(i => {  // for every team in the response
+        const checkedPositions = choosePositions();
 
-        const playerImage = document.createElement('img');
-        playerImage.src = i.player.photo;
-        playerImage.alt = i.player.firstname + " " + i.player.lastname;
-        playerCard.appendChild(playerImage);
-
-        const playerDetails = document.createElement('div');
-        playerDetails.className = 'player-details';
-
-        const playerName = document.createElement('h3');
-        playerName.textContent = i.player.firstname + " " + i.player.lastname;
-        playerDetails.appendChild(playerName);
-
-        const playerPosition = document.createElement('h3');
-        playerPosition.textContent = "Position: " + i.statistics[0].games.position;
-        playerDetails.appendChild(playerPosition);
-
-        const playerAge = document.createElement('h3');
-        playerAge.textContent = "Age: " + i.player.age;
-        playerDetails.appendChild(playerAge);
-
-        const playerNationality = document.createElement('h3');
-        playerNationality.textContent = "Nationality: " + i.player.nationality;
-        playerDetails.appendChild(playerNationality);
-
-        const playerAppearences = document.createElement('h3');
-        playerAppearences.textContent = `Appearances in the ${year} premier division: ${i.statistics[0].games.appearences}`;
-        playerDetails.appendChild(playerAppearences);
-
-        playerCard.appendChild(playerDetails);
-        playerContainer.appendChild(playerCard);
+        if (checkedPositions.includes(i.statistics[0].games.position)) {
+            const playerCard = document.createElement('div');
+            playerCard.classList = 'player-card';
+    
+            const playerImage = document.createElement('img');
+            playerImage.src = i.player.photo;
+            playerImage.alt = i.player.firstname + " " + i.player.lastname;
+            playerCard.appendChild(playerImage);
+    
+            const playerDetails = document.createElement('div');
+            playerDetails.className = 'player-details';
+    
+            const playerName = document.createElement('h3');
+            playerName.textContent = i.player.firstname + " " + i.player.lastname;
+            playerDetails.appendChild(playerName);
+    
+            const playerPosition = document.createElement('h3');
+            playerPosition.textContent = "Position: " + i.statistics[0].games.position; // statistics is divivied by leagues, but premier division is alwats first, so it's set to check [0]
+            playerDetails.appendChild(playerPosition);
+    
+            const playerAge = document.createElement('h3');
+            playerAge.textContent = "Age: " + i.player.age;
+            playerDetails.appendChild(playerAge);
+    
+            const playerNationality = document.createElement('h3');
+            playerNationality.textContent = "Nationality: " + i.player.nationality;
+            playerDetails.appendChild(playerNationality);
+    
+            const playerAppearences = document.createElement('h3');
+            playerAppearences.textContent = `Appearances in the ${year} premier division: ${i.statistics[0].games.appearences}`;
+            playerDetails.appendChild(playerAppearences);
+    
+            playerCard.appendChild(playerDetails);
+            playerContainer.appendChild(playerCard);
+        }
     });
 }
 
@@ -145,4 +152,19 @@ async function fetchPlayersFromTeam(teamID) {
     } catch (error) {
         console.error(error);
     }
+}
+
+function choosePositions() {
+    const checkedPositions = []; // list for checked boxes
+    
+    if (document.getElementById("GoalkeeperCheckbox").checked) // if this checkbox is checked...
+        checkedPositions.push("Goalkeeper"); // add the position to the list
+    if (document.getElementById("DefenderCheckbox").checked)
+        checkedPositions.push("Defender");
+    if (document.getElementById("MidfielderCheckbox").checked)
+        checkedPositions.push("Midfielder");
+    if (document.getElementById("AttackerCheckbox").checked)
+        checkedPositions.push("Attacker");
+
+    return checkedPositions; // return the list
 }
